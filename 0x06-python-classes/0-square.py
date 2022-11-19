24ce121074abde3d428c1e9f6a78a5524f073d23
Nando-jr <ebonghooladapo@yahoo.com>
2022-11-18 22:59:51 -0800

    Tasks 0-6

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    '''An empty class Square that defines a square
+    '''
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size):
+        """
+            Instantiation with size
+        Args:
+            size: size of a square
+        """
+        self.__size = size
