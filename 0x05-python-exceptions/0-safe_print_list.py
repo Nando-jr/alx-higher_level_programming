b7329a93fe2b4094df209ccf0aec980bb6e5ffc3
Nando-jr <ebonghooladapo@yahoo.com>
2022-11-18 22:17:41 -0800

    Tasks 0-6

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    p = 0
+    for n in range(x):
+        try:
+            print("{}".format(my_list[n]), end="")
+        except Exception:
+            break
+        else:
+            p += 1
+    print()
+    return p
