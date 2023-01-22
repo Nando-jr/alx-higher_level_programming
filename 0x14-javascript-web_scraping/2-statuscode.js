db9eb798d808148dfd3cc7244012eb71fadb1c86
nando-jr <ebonghooladapo@yahoo.com>
2023-01-22 04:13:32 +0000

    Task 2-6

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,7 @@
-t url = process.argv[2];
+#!/usr/bin/node
+// A script that display the status code of a GET request
+
+const url = process.argv[2];
 const request = require('request');
 
 request(url, function (error, response, body) {
